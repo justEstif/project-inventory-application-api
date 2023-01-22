6b43b3d157a19797f6c3fddc9857ab30c6ff5d1a
Estifanos <estifanosbeyene21@gmail.com>
2023-01-22 17:15:52 -0500

    docs: clean up tsconfig

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,15 @@ import express from "express";
 import routes from "./routes";
 import env from "./config/env";
 
-const { PORT } = env;
+const { PORT, NODE_ENV } = env;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+  if (NODE_ENV !== "production") {
+    console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+  }
   routes(app);
 });
